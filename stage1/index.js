de1e080b777e14420e66fe6306a1b599dd308fef
samuelfreeman <samuelmanueljnr@gmail.com>
2024-07-02 02:35:41 +0000

    fixing error

--- stage1/index.js
+++ stage1/index.js
@@ -8,18 +8,12 @@ const mainrouter = require("./router");
 
 require("dotenv").config();
 
-app.use(cors());
+app.set("trust proxy", true);
 
-app.use((error, req, re));
+app.use(cors());
 
 app.use("/api", mainrouter);
 
-app.use((err, req, res, next) => {
-  res.status(err.status || 500).json({
-    status: err.status || 500,
-    error: err.message,
-  });
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
